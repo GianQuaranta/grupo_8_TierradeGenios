f9af915b975286df54bf29808514820228576862
AGardey <agustingardey@gmail.com>
2021-06-17 18:32:45 -0300

    se corrigió userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,19 +1,15 @@
 const userController = {
     register: function(req,res){
         return res.render('register')
-    }
-},
 
-const userController = {
+    },
     login: function(req,res){
         return res.render('login')
-    }
-},
-
-const userController = {
+    },
     contrasenia: function(req,res){
         return res.render('contrasenia')
     }
+
 }
 
 module.exports = userController;
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,6 +3,7 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.get('/register' , userController.register);
+router.post('/register' , userController.create);
 router.get('/login' , userController.login);
 router.get('/contrasenia' , userController.contrasenia);
 
