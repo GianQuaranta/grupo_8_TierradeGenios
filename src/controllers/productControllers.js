afc25696173debcbb89660b34c90d168695b3e23
GianQuaranta <gianquarantaa@gmail.com>
2021-06-21 22:03:10 -0300

    vistas de creación y edición de producto realizadas

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -5,7 +5,22 @@ const productController = {
 
     productList: (req,res) =>{
         res.render('productList')
+    },
+    create: (req,res) => {
+        res.render("productCreationForm")
+    },
+    saveProduct: (req,res) => {
+        //Falta la lógica
+        res.send("Falta la lógica")
+    },
+    edit: (req,res) => {
+        res.render("productEditingForm")
+    },
+    update: (req,res) => {
+        //Falta la lógica
+        res.send("Falta la lógica")
     }
+
 }
 
 module.exports = productController;
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -5,6 +5,10 @@ const router = express.Router();
 
 router.get('/productDetail' , productController.vistaDeLosProductos)
 router.get('/productList' , productController.productList);
+router.get("/productCreationForm", productController.create);
+router.post("/productCreationForm", productController.saveProduct);
+router.get("/productEditingForm", productController.edit);
+router.put("/productEditingForm", productController.update);
 
 
 module.exports = router;
